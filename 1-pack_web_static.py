9116b40addab9d981878a7a52c1e5c5ab7e4d56b
masiDaniel <masidaniel02@gmail.com>
2024-03-04 16:57:29 +0300

    fab

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive
+from the contents of the web_static folder"""
+from time import strftime
+from fabric.api import local, task
+
+
+@task
+def do_pack():
+    """Function to compress files"""
+    file = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(file))
+
+        return "versions/web_static_{}.tgz".format(file)
+
+    except Exception as e:
+        return None
